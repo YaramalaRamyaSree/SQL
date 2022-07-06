92d32a21a5a9fe86baff0d6e3c0f98af44d29295
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-07-07 00:15:01 +0530

    Update DepartmentHigheestSalary.sql

--- DepartmentHigheestSalary.sql
+++ DepartmentHigheestSalary.sql
@@ -1,3 +1,10 @@
 /*
 leetcode link -https://leetcode.com/problems/department-highest-salary/
 */
+
+/*Print all employees who have highest salary in each department there may be a case in suppose IT department has two or more employees having same highest salary */
+
+SELECT Department.Name AS Department , Employee.Name AS Employee ,Salary FROM  EMPLOYEE
+JOIN Department ON Employee.departmentId=Department.id
+WHERE (DepartmentId,Salary ) IN
+(SELECT DepartmentId, MAX(Salary) FROM Employee GROUP BY DepartmentId);
