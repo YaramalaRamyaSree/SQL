89f6b772a43ec32d53bb5d20fd5066efe48da87e
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-07-08 00:07:41 +0530

    Update RisingTemperature.sql

--- RisingTemperature.sql
+++ RisingTemperature.sql
@@ -1,3 +1,7 @@
 /*
 leetcode link - https://leetcode.com/problems/rising-temperature/
 */
+
+#DATEDIFF 
+SELECT w2.id FROM Weather w1 JOIN Weather w2 on DATEDIFF(w1.recordDate,w2.recordDate)=-1
+WHERE w1.temperature<w2.temperature;
